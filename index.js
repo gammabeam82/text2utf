bb9c0fc6b51202693ae5904bd52684ff4dbdac45
Paul Mastavy <gammabeam82@gmail.com>
2018-03-28 14:00:21 +0300

    Update app

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const convertEncoding = require('encoding');
 const util = require('util');
 const process = require('process');
 const commandLineArgs = require('command-line-args');
+const path = require('path');
 
 const getFiles = util.promisify(fs.readdir);
 const getContent = util.promisify(fs.readFile);
@@ -17,6 +18,8 @@ const optionDefinitions = [
   { name: 'dir', type: String, multiple: false, defaultOption: true },
   { name: 'copy', type: Boolean }
 ];
+
+const types = ['application/x-subrip', 'text/plain'];
 
 const convertData = (data, encoding) => {
   return new Promise((resolve, reject) => {
@@ -30,16 +33,12 @@ const convertData = (data, encoding) => {
 
 const prepareList = (files, dir) => {
   return files
-    .filter(file => fs.lstatSync(dir.concat(file)).isFile())
-    .filter(file => {
-      let type = mime.getType(dir.concat(file));
-      return 'text/plain' === type || 'application/x-subrip' === type
-    })
+    .filter(file => fs.lstatSync(path.join(dir, file)).isFile())
+    .filter(file => types.includes(mime.getType(path.join(dir, file))))
     .map(file => {
-      let fullpath = dir.concat(file);
+      let fullpath = path.join(dir, file);
       let { encoding } = detectCharacterEncoding(readChunk.sync(fullpath, 0, 4096));
       return {
-        filename: file,
         fullpath,
         encoding
       }
@@ -52,11 +51,12 @@ const processList = (list, saveDir, options) => {
   });
 
   list.forEach(async (item) => {
-    let { filename, fullpath, encoding } = item;
+    let { fullpath, encoding } = item;
+    let name = path.join(saveDir, path.basename(fullpath));
 
     if ('UTF-8' === encoding) {
       if (options.copy) {
-        await copy(fullpath, saveDir.concat(filename));
+        await copy(fullpath, name);
       }
       bar.tick();
       return;
@@ -65,15 +65,22 @@ const processList = (list, saveDir, options) => {
     let data = await getContent(fullpath);
     let convertedData = await convertData(data, encoding);
 
-    await putContent(saveDir.concat(filename), convertedData);
+    await putContent(name, convertedData);
 
     bar.tick();
   });
 }
 
 const checkFs = (dir, saveDir) => {
-  if (!fs.existsSync(dir)) {
+  if (!fs.existsSync(dir) || !fs.lstatSync(dir).isDirectory()) {
     console.log(`Invalid path: ${dir}`);
+    process.exit(1);
+  }
+
+  try {
+    fs.accessSync(dir, fs.constants.R_OK && fs.constants.W_OK);
+  } catch (err) {
+    console.log('Access denied');
     process.exit(1);
   }
 
@@ -83,13 +90,8 @@ const checkFs = (dir, saveDir) => {
 }
 
 const run = options => {
-  let dir = options.dir || './';
-
-  if (!dir.match(/(.)\/$/)) {
-    dir = dir.concat('/');
-  }
-
-  const saveDir = dir.concat('processed/');
+  const dir = options.dir || './';
+  const saveDir = path.join(dir, 'processed');
 
   checkFs(dir, saveDir);
   getFiles(dir)
